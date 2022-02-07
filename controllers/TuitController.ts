0b7809131de42bebf36fbc4f0d055d25df7aaf2e
nandakishore2610 <kasula.n@northeastern.edu>
2022-02-07 17:56:32 -0500

    TuitControllerImplementation

---
+++ controllers/TuitController.ts
@@ -0,0 +1,41 @@
+import {Request, Response, Express} from "express";
+import TuitDao from "../daos/TuitDao";
+import TuitControllerI from "../interfaces/TuitController";
+import Tuit from "../models/Tuit";
+
+export default class TuitController implements TuitControllerI {
+    private static tuitDao: TuitDao = TuitDao.getInstance();
+    private static tuitController: TuitController | null = null;
+    public static getInstance = (app: Express): TuitController => {
+        if(TuitController.tuitController === null) {
+            TuitController.tuitController = new TuitController();
+            app.get('/api/tuits', TuitController.tuitController.findAllTuits);
+            app.get('/api/tuits/:tid', TuitController.tuitController.findTuitById);
+            app.get("/api/users/:uid/tuits", TuitController.tuitController.findTuitsByUser);
+            app.post("/api/users/:uid/tuits", TuitController.tuitController.createTuit);
+            app.put("/api/tuits/:uid", TuitController.tuitController.updateTuit);
+            app.delete("/api/tuits/:uid", TuitController.tuitController.deleteTuit);
+            
+        }
+        return TuitController.tuitController;
+   }
+   private constructor(){}
+   findAllTuits = (req: Request, res: Response) =>
+        TuitController.tuitDao.findAllTuits()
+           .then((tuits:Tuit[]) => res.json(tuits));
+    findTuitsByUser = (req: Request, res: Response) =>
+        TuitController.tuitDao.findTuitsByUser(req.params.uid)
+            .then((tuits: Tuit[]) => res.json(tuits));
+   findTuitById = (req: Request, res: Response) =>
+        TuitController.tuitDao.findTuitById(req.params.tid)
+            .then((tuit: Tuit) => res.json(tuit));
+    createTuit = (req: Request, res: Response) =>
+        TuitController.tuitDao.createTuit(req.params.uid, req.body)
+            .then(tuit => res.json(tuit));
+    deleteTuit = (req: Request, res: Response) =>
+        TuitController.tuitDao.deleteTuit(req.params.tid)
+            .then(status => res.json(status));
+    updateTuit = (req: Request, res: Response) =>
+        TuitController.tuitDao.updateTuit(req.params.tid, req.body)
+            .then(status => res.json(status));
+}
---
+++ daos/TuitDao.ts
@@ -0,0 +1,33 @@
+import Tuit from "../models/Tuit"
+import TuitDaoI from "../interfaces/TuitDao"
+import TuitModel from "../mongoose/TuitModel"
+
+export default class TuitDao implements TuitDaoI {
+    private static tuitDao: TuitDao | null = null;
+    public static getInstance = (): TuitDao => {
+        if(TuitDao.tuitDao === null) {
+            TuitDao.tuitDao = new TuitDao();
+        }
+        return TuitDao.tuitDao;
+    }
+    private constructor() {}
+    async findAllTuits(): Promise<Tuit[]> {
+        return await TuitModel.find();
+    }
+    async findTuitsByUser(uid: string): Promise<any> {
+        return await TuitModel.find({postedBy:uid});
+    }
+    async findTuitById(tid: string): Promise<any> {
+        return await TuitModel.findById(tid);
+    }
+    async createTuit(tuit: Tuit): Promise<any> {
+        return await TuitModel.create(tuit);
+    }
+    async updateTuit(tid: string, tuit: Tuit): Promise<any> {
+        return await TuitModel.updateOne({_id: tid}, {$set: tuit});
+    }
+    async deleteTuit(tid: string): Promise<any> {
+        return await TuitModel.deleteOne({_id:tid});
+    }
+  
+}
