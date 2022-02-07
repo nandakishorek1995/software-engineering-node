9598eceb2120f868d8c07eeb5ab1ff679d529e3a
nandakishore2610 <kasula.n@northeastern.edu>
2022-02-07 18:00:55 -0500

    updated files

--- controllers/TuitController.ts
+++ controllers/TuitController.ts
@@ -7,26 +7,26 @@ export default class TuitController implements TuitControllerI {
     private static tuitDao: TuitDao = TuitDao.getInstance();
     private static tuitController: TuitController | null = null;
     public static getInstance = (app: Express): TuitController => {
-        if(TuitController.tuitController === null) {
-            TuitController.tuitController = new TuitController();
-            app.get('/api/tuits', TuitController.tuitController.findAllTuits);
-            app.get('/api/tuits/:tid', TuitController.tuitController.findTuitById);
-            app.get("/api/users/:uid/tuits", TuitController.tuitController.findTuitsByUser);
-            app.post("/api/users/:uid/tuits", TuitController.tuitController.createTuit);
-            app.put("/api/tuits/:uid", TuitController.tuitController.updateTuit);
-            app.delete("/api/tuits/:uid", TuitController.tuitController.deleteTuit);
-            
-        }
-        return TuitController.tuitController;
-   }
-   private constructor(){}
-   findAllTuits = (req: Request, res: Response) =>
+            if(TuitController.tuitController === null) {
+                TuitController.tuitController = new TuitController();
+                app.get('/api/tuits', TuitController.tuitController.findAllTuits);
+                app.get('/api/tuits/:tid', TuitController.tuitController.findTuitById);
+                app.get("/api/users/:uid/tuits", TuitController.tuitController.findTuitsByUser);
+                app.post("/api/users/:uid/tuits", TuitController.tuitController.createTuit);
+                app.put("/api/tuits/:tid", TuitController.tuitController.updateTuit);
+                app.delete("/api/tuits/:tid", TuitController.tuitController.deleteTuit);
+                
+            }
+            return TuitController.tuitController;
+    }
+    private constructor(){}
+    findAllTuits = (req: Request, res: Response) =>
         TuitController.tuitDao.findAllTuits()
            .then((tuits:Tuit[]) => res.json(tuits));
     findTuitsByUser = (req: Request, res: Response) =>
         TuitController.tuitDao.findTuitsByUser(req.params.uid)
             .then((tuits: Tuit[]) => res.json(tuits));
-   findTuitById = (req: Request, res: Response) =>
+    findTuitById = (req: Request, res: Response) =>
         TuitController.tuitDao.findTuitById(req.params.tid)
             .then((tuit: Tuit) => res.json(tuit));
     createTuit = (req: Request, res: Response) =>
