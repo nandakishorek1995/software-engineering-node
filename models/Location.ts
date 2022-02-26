40b066f7f63f97087875485bb6120406ff2a919e
nandakishore2610 <kasula.n@northeastern.edu>
2022-02-25 19:31:28 -0500

    Assignment 2

--- models/Location.ts
+++ models/Location.ts
@@ -1,3 +1,7 @@
+
+/**
+ * @file Location class which has information about the latitude and longitude.
+ */
 export default class Location {
     public latitude: number = 0.0;
     public longitude: number = 0.0;
